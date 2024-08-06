c37aa356d4fa251fcd769723f3b2c1fa37662fca
TheDanielV <126925561+TheDanielV@users.noreply.github.com>
2024-08-06 18:34:40 -0500

    Fix user route

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from app.api.v1.endpoints import user, auth, auth_service, vehicle
 
 app = FastAPI()
 
-app.include_router(gateway.router, prefix="/api/v1", tags=["gateway"])
+app.include_router(user.router, prefix="/api/v1", tags=["user"])
 app.include_router(vehicle.router, prefix="/api/v1", tags=["vehicle"])
 app.include_router(auth_service.router, prefix="/api/v1", tags=["auth_service"])
 app.include_router(auth.router, prefix="/api/v1", tags=["auth"])
